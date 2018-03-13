06d7c502864032b44ecf0208aaf4cc3fe4a4df0d
joelchu <joelckchu@gmail.com>
2018-03-13 13:08:35 +0800

    adding the callback options

--- index.js
+++ index.js
@@ -5,6 +5,24 @@
  */
 const provider = require('./lib/providers');
 const defaultOptions = require('./lib/option');
+const log = require('./lib/log');
+const { spawn } = require('child_process');
+
+// Create a callback to execute
+const createCallback = function(cmd) {
+  return function(payload, opt) {
+    const process = spawn(cmd[0], cmd.filter((c, i) => i > 0), opt);
+    process.stdout.on('data', data => {
+      log(`cmd stdout: ${data}`);
+    });
+    process.stderr.on('data', data => {
+      log(`cmd stderr: ${data}`);
+    });
+    process.on('close', code => {
+      log(`cmd exited with ${code}`);
+    });
+  };
+};
 /**
  * @param {object} config configuration for the function return
  * @return {function} for calls
@@ -17,17 +35,19 @@ const serve = options => {
   if (!config.secret || config.secret === '') {
     throw new Error('You must provide the secret!');
   }
+  if (typeof config.cmd !== 'string' && typeof config.cmd !== 'function') {
+    throw new Error('Cmd must be a string or a function!');
+  }
   const createHandler = provider(config.provider);
-  /**
-   * @TODO allow passing an function to the cmd?
-   */
-  config.cmd = config.cmd.split(' ');
-  const opt = {
-    env: Object.assign({}, process.env),
-    cwd: config.dir ? config.dir : process.cwd()
-  };
   // Return without Promise, because there is no need to
-  return createHandler(config, opt);
+  return createHandler(
+    config,
+    {
+      env: Object.assign({}, process.env),
+      cwd: config.dir ? config.dir : process.cwd()
+    },
+    typeof config.cmd === 'function' ? config.cmd : createCallback(config.cmd.split(' '))
+  );
 };
 // Export
 module.exports = serve;
