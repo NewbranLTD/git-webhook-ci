be0a161fbdac25f83aad42b16369dec07b5e9d2f
joelchu <joelckchu@gmail.com>
2018-03-21 14:47:40 +0800

    complete the first test

--- __tests__/wechat.test.js
+++ __tests__/wechat.test.js
@@ -6,21 +6,35 @@ const nothing = () => {};
 const supertest = require('supertest');
 const sha1 = require('sha1');
 const gitWebhookCi = require('../index');
+const globalConfig = require('./fixtures/config.json');
 const debug = require('debug')('git-webhook-ci:test');
-
 const { getTimestamp, getRandomInt } = require('../lib/lib/helpers');
-
-describe('Test Wechat mini callback interface', () => {
-  const timestamp = getTimestamp();
-  const nonce = getRandomInt(1,10);
-  let signature;
-  let server;
-  let ctn = 0;
-  let config = {
+const nonce = getRandomInt(1,10);
+const timestamp = getTimestamp();
+const echostr = 'knock knock';
+let config = Object.assing(
+  {
     secret: 'the-token-setup-with-wechat',
     provider: 'wechat',
-    inited: false
-  };
+    inited: false,
+    port: 8082
+  },
+  globalConfig,
+  {
+    cmd: (payload, opt) => {
+      debug('Execute gitee callback');
+      nothing(opt);
+    }
+  }
+);
+const host = [config.host, config.port].join(':');
+// Run test
+describe('Test Wechat mini callback interface', () => {
+  const tmpArr = [config.secret, timestamp, nonce];
+  tmpArr.sort();
+  const signature = sha1(tmpArr.join(''));
+  let server;
+  let ctn = 0;
   // There will be two test here, first test the verify method
   beforeEach(() => {
     if (ctn > 0) {
@@ -35,8 +49,15 @@ describe('Test Wechat mini callback interface', () => {
     server.close();
   });
 
-  test('It should able to verify the wechat GET with query strings', () => {
-
-
+  test('It should able to verify the wechat GET with query strings', done => {
+    supertest(host)
+      .get(config.path)
+      .query({ signature, timestamp, nonce, echostr })
+      .set('Accept', 'application/json')
+      .expect(200, /echostr/)
+      .end(err => {
+        debug('err', err);
+        done();
+      });
   });
 });
