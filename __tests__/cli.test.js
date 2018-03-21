ea4a302db83f4d11704bdd26a20476774cadf322
joelchu <joelckchu@gmail.com>
2018-03-21 16:36:26 +0800

    cli test with some timing error

--- __tests__/cli.test.js
+++ __tests__/cli.test.js
@@ -1,4 +1,57 @@
 'use strict';
+jest.setTimout = 10000;
 /**
  * CLI test
  */
+const { spawn } = require('child_process');
+const nothing = () => {};
+const supertest = require('supertest');
+const payload = require('./fixtures/gitee-payload.json');
+const globalConfig = require('./fixtures/config.json');
+const gitWebhookCi = require('../index');
+const debug = require('debug')('git-webhook-ci:test');
+const { join } = require('path');
+const cli = join(__dirname, '..', 'cli.js');
+const config = Object.assign(
+  {
+    provider: 'gitee'
+  },
+  globalConfig,
+  {
+    cmd: (payload, opt) => {
+      debug('Execute gitee callback');
+      nothing(opt);
+    }
+  }
+);
+const host = [config.host, config.port].join(':');
+// Run
+describe('Testing with gitee provider using the cli', () => {
+  let server;
+  beforeAll(done => {
+    server = spawn('node', [cli]);
+    server.stdout.on('data', data => {
+      debug(`stdout ${data}`);
+    });
+    done();
+  });
+
+  afterAll(() => {
+    server.kill();
+  });
+
+  it('It should respond with a push payload', done => {
+    supertest(host)
+      .post(config.path)
+      .set('Accept', 'application/json')
+      .send(payload.push)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(err => {
+        if (err) {
+          debug('err', err);
+        }
+        done();
+      });
+  });
+});
